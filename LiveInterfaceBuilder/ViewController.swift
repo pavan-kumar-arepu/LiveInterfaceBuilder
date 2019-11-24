cff495a7b8d2724363e6211668ee6247f66dece0
Pavankumar arepu <iosdeveloper.ipa@gmail.com>
2019-11-24 23:31:18 +0530

    Initial Commit

---
+++ LiveInterfaceBuilder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveInterfaceBuilder
+//
+//  Created by Sivarama Krishna on 24/11/19.
+//  Copyright © 2019 Arepu PavanKumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
